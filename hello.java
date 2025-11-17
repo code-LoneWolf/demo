fd271abf477e082800c5d14375e128c2bff30954
GOKUL <gokul.r.m.102003@gmail.com>
2025-11-17 18:06:15 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class hello{
     public static void main(String[] args){
         int i = 1;
 
-        while(i <= 5){
+        while(i <= 10){
             System.out.println("Count up: " + i);
             i++;
         }
